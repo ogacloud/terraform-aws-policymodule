0bdff855d1a8781849ca2ff0fd59546290254d61
amsanmie <sanmiej@gmail.com>
2021-03-10 10:08:47 +0000

    terraform-policy-module

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_policy" "policy" {
+  name        = var.name
+  path        = "/"
+  description = var.description
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
